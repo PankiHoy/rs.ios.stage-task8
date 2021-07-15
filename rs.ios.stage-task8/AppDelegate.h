1c07679949592606f3890ad4e29d3497a10e2b5f
Maksim Karaseu <dassive_mick@mail.ru>
2021-07-15 23:01:58 +0300

    Initial Commit

---
+++ rs.ios.stage-task8/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  rs.ios.stage-task8
+//
+//  Created by dev on 15.07.21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ rs.ios.stage-task8/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  rs.ios.stage-task8
+//
+//  Created by dev on 15.07.21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
